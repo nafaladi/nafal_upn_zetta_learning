eef2fefc1b15e720af1cb356b6407d16e57ec2f4
nafaladi <adisyamaidzar2@gmail.com>
2022-10-28 15:26:34 +0700

    Add day2.ts

--- day1.js
+++
@@ -1,8 +0,0 @@
-function Slicer(text, first_index, last_index) {
-    var hasil = "";
-    for (var i = first_index; i < last_index; i++) {
-        hasil += text[i];
-    }
-    return hasil;
-}
-console.log(Slicer("Learning Typescript is different than Javascript", 9, 19));
