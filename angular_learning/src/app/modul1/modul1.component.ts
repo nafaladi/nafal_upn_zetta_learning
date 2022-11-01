ec30535b3d6462bec4f039de035826b3c92ce551
nafaladi <adisyamaidzar2@gmail.com>
2022-11-02 06:42:59 +0700

    Add modul1

--- angular_learning/src/app/modul1/modul1.component.ts
+++ angular_learning/src/app/modul1/modul1.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-modul1',
   templateUrl: './modul1.component.html',
-  styleUrls: ['./modul1.component.css']
+  styleUrls: ['./modul1.component.scss']
 })
 export class Modul1Component implements OnInit {
 
