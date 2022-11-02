8fb068a19c8b86cbf552e4d18b2b18f8a03234b0
nafaladi <adisyamaidzar2@gmail.com>
2022-11-03 00:16:44 +0700

    Add items, items-input

--- angular_learning/src/app/app.component.ts
+++ angular_learning/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular_learning';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  taskElements = [{type:'individu',name:'Contoh',content:'Buat tugas angular_day3'}];
+ 
+  onTaskAdded(taskData: {taskName:string,taskContent:string}) {
+    this.taskElements.push({
+      type: 'individu',
+      name: taskData.taskName,
+      content: taskData.taskContent
+    });
+  }
+
+  onGroupTaskAdded(groupTaskData: {taskName:string,taskContent:string}) {
+    this.taskElements.push({
+      type: 'kelompok',
+      name: groupTaskData.taskName,
+      content: groupTaskData.taskContent
+    });
+  }
+
+  onDestroy(){
+    this.taskElements.splice(0,100);
+  }
+
+
 }
--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemsInputComponent } from './items-input/items-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ItemsInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
