717013701047e72b670754cc871b0e98aed0790f
nafaladi <adisyamaidzar2@gmail.com>
2022-11-02 08:19:44 +0700

    Add cards

---
+++ angular_learning/src/app/cards/cards.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.scss']
+})
+export class CardsComponent implements OnInit {
+  user = ['Paijo','Sutiyah','Painem'];
+  currentStatus1 = 'offline';
+  currentStatus2 = 'offline';
+  currentStatus3 = 'offline';
+  status = true;
+
+  changeStatus1(){
+    this.currentStatus1 = "online";
+  }
+  changeStatus2(){
+    this.currentStatus2 = "online";
+  }
+  changeStatus3(){
+    this.currentStatus3 = "online";
+  }
+
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+  }
+
+}
